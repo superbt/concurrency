5b654baaf110d33f0dd8039adabd88251c4bdb89
bt <dys456123@163.com>
2024-02-23 19:04:51 +0800

    线程控制

---
+++ src/bt/code/sf/code/SequentialPrinter.java
@@ -0,0 +1,60 @@
+package bt.code.sf.code;
+
+public class SequentialPrinter {
+
+    private static final Object lock = new Object();
+    private static int number = 1 ;
+    private static  final  int MAX_NUMBER = 100 ;
+
+    public static void main(String[] args) {
+        Thread oddThread = new Thread(new OddPrinter());
+        Thread evenThread = new Thread(new EvenPrinter());
+        oddThread.start();
+        evenThread.start();
+    }
+
+    static  class  OddPrinter implements  Runnable{
+
+        @Override
+        public void run() {
+            while (number<MAX_NUMBER){
+                synchronized (lock){
+                    if(number%2==0){
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }else{
+                        System.out.println(String.format("这是奇数%s",number));
+                        number++;
+                        lock.notify();
+                    }
+                }
+            }
+        }
+    }
+
+    static class  EvenPrinter implements  Runnable{
+
+        @Override
+        public void run() {
+            while ((number<=MAX_NUMBER)){
+                synchronized (lock){
+
+                    if(number%2==1){
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }else {
+                        System.out.println(String.format("这是偶数%s",number));
+                        number++ ;
+                        lock.notify();
+                    }
+                }
+            }
+        }
+    }
+}
