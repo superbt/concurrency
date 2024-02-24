d1c33ae43b36dcf95ddfe6e3f1e5a37f87caec44
bt <dys456123@163.com>
2024-02-24 17:39:52 +0800

    线程控制

---
+++ src/bt/code/sf/code/ThreadContrilTest.java
@@ -0,0 +1,13 @@
+package bt.code.sf.code;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+
+public class ThreadContrilTest {
+
+//    public static void main(String[] args) {
+//        ScheduledExecutorService backListCheckExecutrorService = new ScheduledThreadPoolExecutor(20,
+//                new ThreadFactory().newThread());
+//    }
+}
