71708168d4c6b193ad9951f736dceffdf9d4c9c0
bt <dys456123@163.com>
2024-02-27 16:27:55 +0800

    设计模式-单利

---
+++ src/bt/code/sf/ds/Singleton2.java
@@ -0,0 +1,12 @@
+package bt.code.sf.ds;
+
+public class Singleton2 {
+    private static  Singleton2  instance = new Singleton2();
+
+    private Singleton2(){}
+
+    public static Singleton2 getInstance() {
+        return instance;
+    }
+
+}
