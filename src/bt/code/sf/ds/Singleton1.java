144d8f3fa4fd8e64b6c9c3ad65ba7e267e4fbeca
bt <dys456123@163.com>
2024-02-27 16:26:10 +0800

    设计模式-单利

---
+++ src/bt/code/sf/ds/Singleton1.java
@@ -0,0 +1,13 @@
+package bt.code.sf.ds;
+
+public class Singleton1 {
+
+    private static Singleton1 singleton ;
+    private Singleton1(){}
+    public static  synchronized Singleton1 getInstance(){
+        if ((singleton==null)){
+            singleton = new Singleton1();
+        }
+        return  singleton ;
+    }
+}
